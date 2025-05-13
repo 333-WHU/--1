50b2f9dc8d042c80f0b2f028239fe19ea556d1ab
333-WHU <147116549+333-WHU@users.noreply.github.com>
2025-05-13 22:54:28 +0800

    一个不完善的版本，但是可以运行

--- deal_matrix.py
+++ deal_matrix.py
@@ -1,9 +1,14 @@
 import pathlib
-import os
 import json
 import numpy as np
+from osgeo import gdal
 
-
+class Point:
+    def __init__(self, x, y,gt):
+        self.x = x
+        self.y = y
+        self.longitude = gt[0] + x * gt[1] + y * gt[2]
+        self.latitude = gt[3] + x * gt[4] + y * gt[5]
 class triple:
     """稀疏矩阵三元组表示的矩阵的一行"""
     def __init__(self, row):
@@ -68,17 +73,16 @@ class triple_list:
             self.max_row = triple.row
         if self.min_row > triple.row:
             self.min_row = triple.row
-    def union(self,triple_list2):
-        """矩阵并集"""
-        for triple2 in triple_list2.triple_list:
-            if self.triple_list == []:
-                self.add(triple2)
-            else:
-                for triple in self.triple_list:
-                    if triple.row == triple2.row:
-                        triple.union(triple2)
-                    else:
-                        self.add(triple2)
+    def union(self,triple2:triple):
+        """矩阵合并一行的数据"""
+        row_list = [t.row for t in self.triple_list]
+        if triple2.row not in row_list:
+            self.add(triple2)
+        else:
+            for i in range(len(self.triple_list)):
+                if self.triple_list[i].row == triple2.row:
+                    self.triple_list[i].union(triple2)
+                    break
     def add_offset(self,offset_x, offset_y):
         """添加偏移量"""
         for triple in self.triple_list:
@@ -101,44 +105,46 @@ class triple_list:
                 if triple.row == triple2.row:
                     result += triple.mutiple(triple2)
         return result
-    def Get_H(self,pt_list:list,side_length = 10):
+    
+    def __getitem__(self, row_col):
+        if len(row_col)==2:
+            row,col = row_col
+            for triple in self.triple_list:
+                if triple.row == row:
+                    for col2,value in triple.col_and_value:
+                        if col2 == col:
+                            return value
+                    return 0
+            return 0
+        else:
+            return 0
+
+    def Get_H(self,pt_list:list[Point],side_length = 10):
         '''根据点集获取足迹矩阵'''
-        H = np.zeros(side_length,side_length)
+        H = triple_list(self.latitude,self.longitude)
         for pt in pt_list:
             # 根据经纬度在矩阵中找到以它为中心10x10区域
-            for t_list in self.triple_list:
-                # 如果pt的经纬度不在t_list的经纬度范围内,跳过
-                if pt.longitude < t_list.longitude-2.7 or pt.longitude > t_list.longitude+2.7 or pt.latitude < t_list.latitude-2.7 or pt.latitude > t_list.latitude+2.7:
-                    continue
-                # 如果pt的经纬度在t_list的经纬度范围内,计算H对应值
-class total_triple_list:
-    """完整的足迹矩阵"""
-    def __init__(self):
-        self.triple_list = []
-    
-    def add(self,triple_list):
-        """添加一个分块矩阵"""
-        self.triple_list.append(triple_list)
-    
-    def union(self)->triple_list:
-        """矩阵并集"""
-        logitude_list = [t.longitude for t in self.triple_list]
-        latitude_list = [t.latitude for t in self.triple_list]
-        # 坐标平均值
-        H = triple_list(np.mean(latitude_list),np.mean(logitude_list))
-        # 初始化分块矩阵
-        for t_list in self.triple_list:
-            # 根据与中心坐标的坐标差，对分块矩阵进行偏移
-            offset_x = int((t_list.latitude - H.latitude) * 10)
-            offset_y = int((t_list.longitude - H.longitude) * 10)
-            t_list.add_offset(offset_x,offset_y)
-            H.union(t_list)
+            # 计算图像坐标
+            center_x = int((pt.longitude-self.longitude) * 10)+135
+            center_y = int((pt.latitude-self.latitude) * 10)+135
+            half = side_length // 2
+            x_left = center_x - half
+            y_top = center_y - half
+            for i in (x_left,x_left+side_length):
+                t = triple(i)
+                for j in (y_top,y_top+side_length):
+                    value = self[i,j]
+                    if value:
+                        t.add(j,value)
+                if t.col_and_value:
+                    H.union(t)
         return H
-
 
 def read_martix(file_folder):
     # 对于所有的json文件,合并为一个完整的矩阵
-    total_triple = total_triple_list()
+    mid_longitude = 112.7
+    mid_latitude = 35.4
+    H = triple_list(mid_latitude,mid_longitude)
     for file in pathlib.Path(file_folder).glob('*.json'):
         with open(file, 'r') as f:
             # 分割filename
@@ -148,25 +154,71 @@ def read_martix(file_folder):
             center_latitude = float(information_list[1])            
             center_longitude = float(information_list[2])
             data = json.load(f)
-            # 对于每一个json文件,生成一个分块矩阵
-            block = triple_list(center_latitude,center_longitude)
+            # 偏移量
+            offset_x = int((center_latitude - H.latitude) * 10)
+            offset_y = int((center_longitude - H.longitude) * 10)
             for row_data in data: # 对每一行进行
                 # 获取行号
-                row = row_data['row']
+                row = row_data['row'] + offset_x
                 # 生成一个triple
                 triple_row = triple(row)
                 # 获取列号和通量值
                 for col,value in row_data['col_and_value']:
+                    col = col + offset_y
                     triple_row.add(col,value)
-                block.add(triple_row)
-            total_triple.add(block)
-    t = total_triple.union()
-    return t
+                H.union(triple_row)
+    return H
             
+
+
+
+class tif:
+    def __init__(self,file_path):
+        self.file_path = file_path
+        self.dataset = gdal.Open(file_path)
+        self.gt = self.dataset.GetGeoTransform()
+        self.band = self.dataset.GetRasterBand(1)
+        self.array = self.band.ReadAsArray()
+        self.width = self.dataset.RasterXSize  # 列数
+        self.height = self.dataset.RasterYSize  # 行数
+        self.box = {'left': self.gt[0],
+                'top': self.gt[3], 
+                'right': self.gt[0] + self.gt[1] * self.width,
+                'bottom': self.gt[3] + self.gt[5] * self.height,
+                'width': self.width,
+                'height': self.height
+                }
+        self.NODATA = self.band.GetNoDataValue()
+    def Get_x(self,pt_list:list[Point],side_length = 10)->triple_list:
+        '''根据图像中坐标获取通量值,x,y为中心行列号，返回长度为10的方阵'''
+        x_h = triple_list(0,0)
+        # 计算矩形范围
+        half = side_length // 2
+        for pt in pt_list:
+            # 根据经纬度在矩阵中找到以它为中心10x10区域
+            # 计算图像坐标
+            center_x = int((pt.longitude-self.box['left']) / self.gt[1])
+            center_y = int((pt.latitude-self.box['top']) / self.gt[5])
+            x_left = max(0,center_x - half)
+            y_top = max(0,center_y - half)
+            x_right = min(self.width,center_x + half)
+            y_bottom = min(self.height,center_y + half)
+            for i in range(x_left,x_right):
+                t = triple(i)
+                for j in range(y_top,y_bottom):
+                    value = self.array[j,i]
+                    if value != self.NODATA:
+                        t.add(j,value)
+                if t.col_and_value:
+                    x_h.union(t)
+        return x_h
+
 
 if __name__ == '__main__':
     from time import time
     start = time()
     total_triple = read_martix('./footprints/')
+    print(total_triple.max_row,np.max([t.max_col for t in total_triple.triple_list]))
+    print(total_triple.min_row,np.min([t.min_col for t in total_triple.triple_list]))
     end = time()
     print(end-start)
--- work.py
+++ work.py
@@ -1,55 +1,69 @@
 import numpy as np
 import pygad
-from deal_tif import Point,tif
-from deal_matrix import triple_list,total_triple_list
+from deal_matrix import *
+import time
 
 # 插值后通量数据读取
-read_tif = tif("./Global_Fuel_Exploitation_Inventory_v2_2019_Total_Fuel_Exploitation.tif")
+read_tif = tif("./out1km.tif")
+# 总体足迹矩阵
+read_H = read_martix("./footprints/")
+i = 0
 def fitness_f(ga_instance,solution, solution_idx):
     '''适应度函数'''
     point_list = solution.tolist()
     pt_list = []
     for i in range(0,5): # 提取基因中五个点的坐标(图像中坐标)
-        point = Point(point_list[2*i],point_list[2*i+1],read_tif)
+        point = Point(point_list[2*i],point_list[2*i+1],read_tif.gt)
         pt_list.append(point)
     # 计算通量
-    x_h = [read_tif.Get_x(pt.x, pt.y) for pt in pt_list]
+    x_h = read_tif.Get_x(pt_list)
     # 计算足迹矩阵
-    
-    
-    
-if 0:
-    # 创建遗传算法实例
-    box = read_tif.box
-    ga_instance = pygad.GA(
-        num_generations=100,
-        num_parents_mating=10,
-        fitness_func=fitness_f,
-        sol_per_pop=50,
-        num_genes=2,
-        gene_type=int,
-        gene_space=[# 五个点的坐标，每个坐标由两个基因表示，取值范围为135到边界的整数
-            {'low': 135, 'high': box["width"]-135,'step':1},
-            {'low': 135, 'high': box["height"]-135,'step':1},
-            {'low': 135, 'high': box["width"]-135,'step':1},
-            {'low': 135, 'high': box["height"]-135,'step':1},
-            {'low': 135, 'high': box["width"]-135,'step':1},
-            {'low': 135, 'high': box["height"]-135,'step':1},
-            {'low': 135, 'high': box["width"]-135,'step':1},
-            {'low': 135, 'high': box["height"]-135,'step':1},
-            {'low': 135, 'high': box["width"]-135,'step':1},
-            {'low': 135, 'high': box["height"]-135,'step':1} 
-            ],
-        parent_selection_type="sss",  # 稳态选择
-        keep_parents=2,
-        crossover_type="single_point",
-        mutation_type="random",
-        mutation_percent_genes=10
-    )
-    # 运行算法
-    ga_instance.run()
-    # 输出结果
-    solution, solution_fitness, _ = ga_instance.best_solution()
-    print(f"最优解: {solution}, 适应度: {solution_fitness}")
-    # 可视化
-    ga_instance.plot_fitness()
+    H = read_H.Get_H(pt_list)
+    # 计算点积
+    y_h = H.mutiple(x_h)
+    # 计算适应度
+    return y_h
+
+# 创建遗传算法实例
+box = read_tif.box
+ga_instance = pygad.GA(
+    num_generations=100,
+    num_parents_mating=10,
+    fitness_func=fitness_f,
+    sol_per_pop=50,
+    num_genes=10,
+    gene_type=int,
+    gene_space=[# 五个点的坐标，每个坐标由两个基因表示，取值范围为135到边界的整数
+        {'low': 135, 'high': box["width"]-135,'step':1},
+        {'low': 135, 'high': box["height"]-135,'step':1},
+        {'low': 135, 'high': box["width"]-135,'step':1},
+        {'low': 135, 'high': box["height"]-135,'step':1},
+        {'low': 135, 'high': box["width"]-135,'step':1},
+        {'low': 135, 'high': box["height"]-135,'step':1},
+        {'low': 135, 'high': box["width"]-135,'step':1},
+        {'low': 135, 'high': box["height"]-135,'step':1},
+        {'low': 135, 'high': box["width"]-135,'step':1},
+        {'low': 135, 'high': box["height"]-135,'step':1} 
+        ],
+    parent_selection_type="sss",  # 稳态选择
+    keep_parents=2,
+    crossover_type="single_point",
+    mutation_type="random",
+    mutation_percent_genes=10
+)
+# 运行算法
+ga_instance.run()
+# 输出结果
+solution, solution_fitness, _ = ga_instance.best_solution()
+# 将图像坐标转换成经纬度坐标
+solution = solution.tolist()
+point_list = []
+for i in range(0,5):
+    point = Point(solution[2*i],solution[2*i+1],read_tif.gt)
+    point_list.append(point)
+print("最优解为：")
+for pt in point_list:
+    print(pt.longitude,pt.latitude)
+print(f"适应度: {solution_fitness}")
+# 可视化
+ga_instance.plot_fitness()
